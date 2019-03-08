e9301619cde94220d82271c6ef1bc66c05e61707
chefkoch24 <kuennecke-felix@web.de>
2019-03-08 11:47:18 +0100

    7

--- tutorium/src/montag/Addition.java
+++ tutorium/src/montag/Addition.java
@@ -3,7 +3,7 @@ package montag;
 public class Addition {
 	public static void main(String[] args) {
 		int a = 3;
-		int b = 5;
+		int b = 7;
 		int ergebnis = a + b;
 		System.out.println("Ergebnis: " + ergebnis);
 	}
